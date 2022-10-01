7c9f5b13283c320a6ec1821b745260a3a15b933a
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2022-10-01 22:44:47 +0800

    Add skeletal get request

--- src/components/VisitorView.jsx
+++ src/components/VisitorView.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  setDoc,
+  getDoc,
+  doc,
+  deleteDoc,
+} from 'firebase/firestore';
+
+import { db } from "../api/firebase-config";
 
 const VisitorView = () => {
+  const [visitors, setVisitors] = useState([]);
+  const visitorRef = async () => {
+    const response = await getDoc(doc(db, "users/dO066zlSg8T7XvoZNttx"));
+    return response;
+  }
+
+  useEffect(() => {
+    visitorRef().then(response => setVisitors({
+      id: response.get("id"),
+      role: response.get("role")
+    }));
+  }, [])
+  
   return (
     <>
       <h2 style={{ textAlign: 'center' }}>Overview of Visitors</h2>
+      <p>
+        {JSON.stringify(visitors)}
+      </p>
     </>
   )
 }
