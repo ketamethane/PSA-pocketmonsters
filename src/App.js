96479d469212131550aad8e415c6d98fa3cd38c7
hauchongtang <56034480+hauchongtang@users.noreply.github.com>
2022-10-01 21:18:43 +0800

    Add skeletal view of dashboard
    Add the interface for Add Visitor button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import DashboardHome from './components/DashboardHome';
 
 function App() {
   return (
-    <div className="App">
-      Initial
+    <div className="App" >
+      <DashboardHome />
     </div>
   );
 }
--- src/components/VisitorEntryView.jsx
+++ src/components/VisitorEntryView.jsx
@@ -1,8 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const VisitorEntryView = () => {
+const VisitorEntryView = ({ toggleAddVisitor }) => {
+  const [locationId, setLocationId] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // submitUser();
+    alert(`You have submitted: ${locationId} and ${userId}`);
+    // If success, else dont toggle
+    toggleAddVisitor(() => false);
+  }
+
   return (
-    <></>
+    <div
+      className='container'
+      style={{
+        display: 'block',
+        textAlign: 'center',
+        backgroundColor: 'green',
+        maxWidth: '1000px',
+        height: '400px',
+        margin: 'auto',
+        transform: 'translateY(50%)'
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          height: '300px',
+          marginTop: 'auto',
+          marginBottom: 'auto'
+        }}
+      >
+        <label style={{ display: 'block' }}>
+          Enter Location ID:
+          <input
+            type='text'
+            value={locationId}
+            onChange={(e) => setLocationId(() => e.target.value)}
+          />
+        </label>
+        <label style={{ display: 'block', marginTop: '0.8em' }}>
+          Enter my ID:
+          <input
+            type='text'
+            value={userId}
+            onChange={(e) => setUserId(() => e.target.value)}
+          />
+        </label>
+        <button
+          disabled={locationId.length === 0 || userId.length === 0}
+          style={{
+            width: '200px',
+            fontSize: 20,
+            marginTop: '0.8em',
+            marginLeft: 'auto',
+            marginRight: 'auto'
+          }}
+          onClick={handleSubmit}
+        >
+          Enter
+        </button>
+      </div>
+
+    </div>
   )
 }
 
